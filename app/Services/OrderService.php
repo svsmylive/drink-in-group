74c958fbe8141fcd6d59935045ee0fb81565c224
Leonid <chakalidi@greensight.ru>
2024-06-29 23:53:29 +0300

    refactoring orderService

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -35,11 +35,15 @@ class OrderService
         $deliveryPrice = data_get($data, 'delivery');
         $userInfo = data_get($data, 'userInfo');
         $userInfo['typeOfDelivery'] = data_get($data, 'typeOfDelivery');
+        $userInfo['delivery'] = $deliveryPrice;
         $totalCost = 0;
         $institution = Institution::find($data['institution_id']);
 
         if ($userInfo['typeOfDelivery'] == 1 && $deliveryPrice == 0) {
-            return response()->json(['error' => "По вашему адресу не смогли рассчитать стоимость доставки, попробуйте выбрать адрес из выпадающего списка"], 400);
+            return response()->json(
+                ['error' => "По вашему адресу не смогли рассчитать стоимость доставки, попробуйте выбрать адрес из выпадающего списка"],
+                400
+            );
         }
 
         foreach ($order as $orderData) {
@@ -86,19 +90,42 @@ class OrderService
             'delivery_type' => $metaData->typeOfDelivery == 1 ? 'Доставка' : 'Самовывоз',
             'firstName' => $metaData->firstName,
             'phone' => $metaData->phone,
-            'address' => $metaData->address ?? '',
+            'address' => $metaData->address ? $metaData->address = str_replace(
+                ' null',
+                ' № квартиры не указан',
+                $metaData->address
+            ) : '',
             'amount' => $order->amount,
+            'comment' => $metaData->comment ?? '',
+            'email' => $metaData->email ?? '',
+            'secondName' => $metaData->secondName ?? '',
+            'delivery' => $metaData->delivery ?? 0,
         ];
 
         $messageOut = 'Заказ с сайта' . "\n";
         $messageOut .= 'Заведение : ' . $data['institution_type'] . ' - ' . $data['institution_name'] . "\n";
         $messageOut .= 'Тип : ' . $data['delivery_type'] . "\n";
-        $messageOut .= 'Имя клиента : ' . $data['firstName'] . "\n";
+        $messageOut .= 'Имя клиента : ' . $data['secondName'] . $data['firstName'] . "\n";
         $messageOut .= 'Телефон : ' . $data['phone'] . "\n";
+
+        if (!empty($data['email'])) {
+            $messageOut .= 'Емайл : ' . $data['email'] . "\n";
+        }
+
         $messageOut .= 'Сумма : ' . $data['amount'] . "\n";
+
+        if ($data['delivery'] > 0) {
+            $messageOut .= 'Цена за доставку : ' . $data['delivery'] . "\n";
+        }
+
         if ($metaData->typeOfDelivery == 1) {
             $messageOut .= 'Адрес : ' . $data['address'] . "\n";
         }
+
+        if (!empty($data['comment'])) {
+            $messageOut .= 'Комментарий : ' . $data['comment'] . "\n";
+        }
+
 
         return $messageOut;
     }
