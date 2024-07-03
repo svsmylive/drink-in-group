dab7005c256f17130547699ade25168afa36f7d1
Leonid <chakalidi@greensight.ru>
2024-07-03 23:33:06 +0300

    Ошибка при интеграции меню из tillypad:

--- app/Console/Commands/MenuImport.php
+++ app/Console/Commands/MenuImport.php
@@ -5,8 +5,9 @@ namespace App\Console\Commands;
 use App\Models\Institution;
 use App\Services\MenuService;
 use App\Services\TillypadService;
-use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class MenuImport extends Command
 {
@@ -36,21 +37,29 @@ class MenuImport extends Command
     protected $description = 'Make import menu from tillypad api';
 
     /**
-     * @throws GuzzleException
      */
-    public function handle()
+    public function handle(): void
     {
         $institutions = [
             Institution::query()->where('type', '=', 'Кулинария')->first(),
             Institution::query()->where('name', '=', 'КАМЕЛОТ')->first()
         ];
 
-        foreach ($institutions as $institution) {
-            $menuCollection = $this->tillypadService->getMenu($institution);
+        try {
+            foreach ($institutions as $institution) {
+                $menuCollection = $this->tillypadService->getMenu($institution);
 
-            if ($menuCollection) {
-                $this->menuService->updateOrCreateFromApi($menuCollection, $institution);
+                if ($menuCollection) {
+                    $this->menuService->updateOrCreateFromApi($menuCollection, $institution);
+                }
             }
+        } catch (\Throwable $e) {
+            $apiKey = config('telegram.api_key');
+
+            Http::post("https://api.telegram.org/bot{$apiKey}/sendMessage", [
+                'chat_id' => '-4281880650',
+                'text' => 'Ошибка при интеграции меню из tillypad: ' . Str::limit($e->getMessage(), 30),
+            ]);
         }
     }
 }
