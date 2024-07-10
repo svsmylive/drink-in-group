f8693e49d1a72737974e42f40bba6c987baec186
Leonid <chakalidi@greensight.ru>
2024-07-10 15:57:51 +0300

    оплата счет смс юкасса

--- app/Services/YookassaService.php
+++ app/Services/YookassaService.php
@@ -123,7 +123,7 @@ class YookassaService implements PaymentInterface
 
                         Http::post("https://api.telegram.org/bot{$apiKey}/sendMessage", [
                             'chat_id' => '-4281880650',
-                            'text' => 'Счет в СМС оплачен успешно! ' . "\n Сумма платежа: " . (float)($payment->amount->value) . "\n, Сумма дохода: " . (float)($payment->income_amount->value) . "\n guestId : " . $guestId,
+                            'text' => 'Счет в СМС оплачен успешно! ' . "\n Сумма платежа: " . (float)($payment->amount->value) . "\n Сумма дохода: " . (float)($payment->income_amount->value) . "\n guestId : " . $guestId,
                         ]);
 
                         Log::channel('yookassa')->debug(
